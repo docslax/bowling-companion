6b1c5a578bcfbd28568325e1af3c8b9e0f7fec42
Joseph Roque <contact@josephroque.ca>
2019-01-27 19:24:14 -0500

    Add mockito and remove DateUtilsInstrumentationTest

--- app/src/test/java/ca/josephroque/bowlingcompanion/utils/DateUtilsTest.kt
+++ app/src/test/java/ca/josephroque/bowlingcompanion/utils/DateUtilsTest.kt
@@ -1,9 +1,16 @@
 package ca.josephroque.bowlingcompanion.utils
 
+import android.content.Context
+import android.content.res.Resources
+import ca.josephroque.bowlingcompanion.R
 import ca.josephroque.bowlingcompanion.TestDates.APR_26_1995
 import ca.josephroque.bowlingcompanion.TestDates.JAN_1_2019
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 import java.util.Calendar
 import java.util.Date
 import java.util.TimeZone
@@ -13,7 +20,34 @@ import java.util.TimeZone
  *
  * Test utility functions in DateUtils
  */
+@RunWith(MockitoJUnitRunner::class)
 class DateUtilsTest {
+
+    @Mock
+    private lateinit var mockContext: Context
+
+    @Mock
+    private lateinit var mockResources: Resources
+
+    @Test
+    fun convertingDateToPretty_IsCorrect() {
+        val yesterday = "Yesterday"
+        val today = "Today"
+        val tomorrow = "Tomorrow"
+
+        `when`(mockContext.resources).thenReturn(mockResources)
+        `when`(mockResources.getString(R.string.yesterday)).thenReturn(yesterday)
+        `when`(mockResources.getString(R.string.today)).thenReturn(today)
+        `when`(mockResources.getString(R.string.tomorrow)).thenReturn(tomorrow)
+
+        DateUtils.init(mockContext)
+
+        assertEquals("January 1, 2019", JAN_1_2019.pretty)
+        assertEquals("April 26, 1995", APR_26_1995.pretty)
+        assertEquals(yesterday, DateUtils.Calendars.yesterday.time.pretty)
+        assertEquals(today, DateUtils.Calendars.today.time.pretty)
+        assertEquals(tomorrow, DateUtils.Calendars.tomorrow.time.pretty)
+    }
 
     @Test
     fun convertingSeriesDateToDate_IsCorrect() {
